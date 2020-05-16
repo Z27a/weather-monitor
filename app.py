cb09093b97aa396eacd9043cdb95873bb90736d2
Z27a <ll29239@tas.qld.edu.au>
2020-05-16 20:43:04 +1000

    dashboard graphs somewhat working

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#TODO fix broken graphs
+#TODO update data tags
+
 #importing libraries
 from flask import *
 import hashlib
@@ -31,23 +34,47 @@ def create_connection(db_file):
     return conn
 
 
-def selectTemp(conn, depth, timestamp):
+def selectTemp(conn, depth, timestamp, repeats, period):
     cur = conn.cursor()
     results = ()
-    for i in range(12):
+    for i in range(repeats):
         sql = f"select {depth} from Link join Temperature on Temperature.TempID = Link.TempID join Dates on Dates.TimeID=Link.TimeID where Dates.timestamps = {timestamp}"
         results = results + cur.execute(sql).fetchall()[0]
-        timestamp = timestamp - 2400
+        timestamp = timestamp - period
+    return(results)
+
+
+def selectWindSpeed(conn, timestamp, repeats, period):
+    cur = conn.cursor()
+    results = ()
+    for i in range(repeats):
+        sql = f"select Windspeed from Link join Wind on Wind.WindID = Link.WindID join Dates on Dates.TimeID=Link.TimeID where Dates.timestamps = {timestamp}"
+        results = results + cur.execute(sql).fetchall()[0]
+        timestamp = timestamp - period
+    return(results)
+
+
+
+def selectWave(conn, timestamp, repeats, period):
+    cur = conn.cursor()
+    results = ()
+    for i in range(repeats):
+        sql = f"select WaveHeight from Link join Wave on Wave.WaveID = Link.WaveID join Dates on Dates.TimeID=Link.TimeID where Dates.timestamps = {timestamp}"
+        results = results + cur.execute(sql).fetchall()[0]
+        timestamp = timestamp - period
     return(results)
 
 #home page
 @app.route("/", methods=('GET', 'POST'))
 def index():
     conn = create_connection(weatherDb)
-    Temp1 = selectTemp(conn, 'Temp1', 1590815510.873754)
-    print(Temp1)
+    Temp1 = selectTemp(conn, 'Temp1', 1590815510.873754, 12, 2400)
+    Temp2 = selectTemp(conn, 'Temp2', 1590815510.873754, 12, 2400)
+    Temp3 = selectTemp(conn, 'Temp3', 1590815510.873754, 12, 2400)
+    WindSpeed = selectWindSpeed(conn, 1590815510.873754, 6, 57600)
+    Wave = selectWave(conn, 1590815510.873754, 13, 7200)
 
-    return render_template('index.html', temps=Temp1)
+    return render_template('index.html', Temp1=Temp1, Temp2=Temp2, Temp3=Temp3, WindSpeed=WindSpeed, Wave=Wave)
 
 if __name__ == '__main__':
     app.run(debug=True)
