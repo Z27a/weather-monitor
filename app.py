d8f5a4363fd14f4f9a918f09e49acf019374d097
Z27a <ll29239@tas.qld.edu.au>
2020-05-24 15:47:25 +1000

    download now downloads all data.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import sqlite3
 from datetime import *
 import ast
 import uuid
+import csv
 
 #setting variables
 app = Flask(__name__)
@@ -78,17 +79,91 @@ def selectNames(conn, email):
     results = cur.execute(sql).fetchall()[0]
     return(results)
 
+def selectNewestTime(conn):
+    cur = conn.cursor()
+    sql = f"select  max(Timestamps) from Dates"
+    results = cur.execute(sql).fetchall()[0][0]
+    return(results)
+
 #home page
 @app.route("/", methods=('GET', 'POST'))
 def index():
     conn = create_connection(db)
-    Temp1 = selectTemp(conn, 'Temp1', 1590815510.873754, 12, 2400)
-    Temp2 = selectTemp(conn, 'Temp2', 1590815510.873754, 12, 2400)
-    Temp3 = selectTemp(conn, 'Temp3', 1590815510.873754, 12, 2400)
-    WindSpeed = selectWindSpeed(conn, 1590815510.873754, 6, 57600)
-    Wave = selectWave(conn, 1590815510.873754, 13, 7200)
+    time = selectNewestTime(conn)
+    Temp1 = selectTemp(conn, 'Temp1', time, 12, 2400)
+    Temp2 = selectTemp(conn, 'Temp2', time, 12, 2400)
+    Temp3 = selectTemp(conn, 'Temp3', time, 12, 2400)
+    WindSpeed = selectWindSpeed(conn, time, 6, 57600)
+    Wave = selectWave(conn, time, 13, 7200)
 
-    return render_template('index.html', Temp1=Temp1, Temp2=Temp2, Temp3=Temp3, WindSpeed=WindSpeed, Wave=Wave)
+    data = {
+        type: 'line',
+        'data': {
+            'labels': [],
+            'datasets': [{
+                'label': '0m',
+                'fill': True,
+                'data': [],
+                'backgroundColor': 'rgba(78, 115, 223, 0.05)',
+                'borderColor': 'rgb(28,200,138)',
+            },
+                {
+                    'label': '-2m',
+                    'fill': True,
+                    'data': [],
+                    'backgroundColor': 'rgba(78, 115, 223, 0.05)',
+                    'borderColor': 'rgb(54,185,204)',
+                },
+                {
+                    'label': '-5m',
+                    'fill': True,
+                    'data': [],
+                    'backgroundColor': 'rgba(78, 115, 223, 0.05)',
+                    'borderColor': 'rgb(54,185,204)',
+                }]
+        },
+        'options': {
+            'maintainAspectRatio': False,
+            'legend': {'display': True},
+            'title': {},
+            'scales': {
+                'xAxes': [{
+                    'gridLines':
+                        {'color': 'rgb(234, 236, 244)',
+                         'zeroLineColor': 'rgb(234, 236, 244)',
+                         'drawBorder': False,
+                         'drawTicks': False,
+                         'borderDash': ['2'],
+                         'zeroLineBorderDash': ['2'],
+                         'drawOnChartArea': True},
+                    'ticks': {
+                        'fontColor': '#858796',
+                        'beginAtZero': False,
+                        'padding': 20}
+                }],
+                'yAxes': [{
+                    'gridLines': {
+                        'color': 'rgb(234, 236, 244)',
+                        'zeroLineColor': 'rgb(234, 236, 244)',
+                        'drawBorder': False,
+                        'drawTicks': False,
+                        'borderDash': ['2'],
+                        'zeroLineBorderDash': ['2']},
+                    'ticks': {
+                        'fontColor': '#858796',
+                        'beginAtZero': False,
+                        'padding': 20}
+                }]
+            }
+        }
+    }
+
+    try:
+        cookie = ast.literal_eval(request.cookies.get('userDetails'))
+        userDetails = cookie[0]
+    except:
+        userDetails = 'Profile'
+    return render_template('index.html', Temp1=Temp1, Temp2=Temp2, Temp3=Temp3, WindSpeed=WindSpeed, Wave=Wave, userDetails=userDetails)
 
 
 @app.route("/temperature", methods=('GET', 'POST'))
@@ -117,9 +192,7 @@ def login():
                     response = make_response(redirect(url_for("index")))
                     # set make cookies with names
                     names = selectNames(conn, email)
-                    print('henlo?')
-                    response.set_cookie(f"{names[0]}", f"{names[1]}")
-                    print('what is this?')
+                    response.set_cookie("userDetails", f"{names[0]} {names[1]}")
                     return response
             except: # if not correct
                 # set error message
@@ -128,6 +201,15 @@ def login():
                 # reload page with error message
                 return render_template('login.html', errorMsg=errorMsg)
     return render_template('login.html')
+
+
+@app.route("/logout")
+def logoutFn():
+    #make cookie, with redirect for main as that is the output page
+    response = make_response(redirect(url_for("index")))
+    #set cookie to nothing, with immediate expiry
+    response.set_cookie('userDetails', '', expires=0)
+    return response
 
 
 #API----------------------------------------------------------------------------------------------------------------------
@@ -190,6 +272,25 @@ def APIgetTemps(repeats=0):
                     </tr>
                     '''
         return html
+
+
+
+@app.route("/api/download/<startDate>/<endDate>", methods=('GET', 'POST'))
+def downloadFile(startDate='', endDate=''):
+    if startDate !='' and endDate != '':
+        with open('output.csv', 'w', newline='') as csvfile:
+            fieldnames = ['Time', 'Temperature (0m)', 'Temperature (-2m)', 'Temperature (-5m)', 'Average']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            conn = create_connection(db)
+            data = APIselectTemp(conn, selectNewestTime(conn), 500, 300)
+            print(data)
+
+            writer.writeheader()
+            for item in data:
+                writer.writerow({'Time':item[4], 'Temperature (0m)':item[0], 'Temperature (-2m)':item[1], 'Temperature (-5m)':item[2], 'Average':item[3]})
+        return  send_file('output.csv', attachment_filename='data.csv')
+
 
 # @app.route("/api/temperature/<repeats>/<orderItem>/<order>", methods=('GET', 'POST'))
 # def APIgetTemps(repeats=0, orderItem=0, order=0):
